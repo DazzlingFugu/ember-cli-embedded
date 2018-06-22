cad2d2fcb979896a6139bf5bf2a7a75b1d5fa551
Xavier Cambar <xcambar@gmail.com>
2018-06-23 00:36:19 +0200

    Creates the service "embedded" that acts as a proxy to the embedded config

---
+++ addon/services/embedded.js
@@ -0,0 +1,15 @@
+import ObjectProxy from '@ember/object/proxy';
+import { getOwner } from '@ember/application';
+
+let configService = ObjectProxy.extend({
+  init() {
+    this.content = getOwner(this).factoryFor('config:embedded').class;
+    this._super(...arguments);
+  }
+});
+
+configService.reopenClass({
+  isServiceFactory: true
+});
+
+export default configService;
