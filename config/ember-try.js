3687d69d7d0b8f91d156a2c83746fca97e1c8531
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2021-04-23 14:57:17 +0100

    chore: restore some root directories
    Restore root directories:
    - `/.github/`
    - `/config/`
    
    The rest of the directories will be restored/updated separately:
    - `/addon/`
    - `/app/`
    - `/tests/`

--- config/ember-try.js
+++ config/ember-try.js
@@ -4,6 +4,7 @@ const getChannelURL = require('ember-source-channel-url');
 
 module.exports = async function () {
   return {
+    useYarn: true,
     scenarios: [
       {
         name: 'ember-lts-3.16',
@@ -43,6 +44,16 @@ module.exports = async function () {
           devDependencies: {
             'ember-source': await getChannelURL('canary'),
           },
+        },
+      },
+      // The default `.travis.yml` runs this scenario via `npm test`,
+      // not via `ember try`. It's still included here so that running
+      // `ember try:each` manually or from a customized CI config will run it
+      // along with all the other scenarios.
+      {
+        name: 'ember-default',
+        npm: {
+          devDependencies: {},
         },
       },
       {
