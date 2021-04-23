b41456c36961f3a93e8346908afcf3c96b518d7d
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2021-04-23 14:57:11 +0100

    chore: delete absolutely everything

--- app/initializers/embedded.js
+++
@@ -1,1 +0,0 @@
-export { default, initialize } from 'ember-cli-embedded/initializers/embedded'
--- types/global.d.ts
+++
@@ -1,6 +0,0 @@
-// Types for compiled templates
-declare module 'ember-cli-embedded/templates/*' {
-  import { TemplateFactory } from 'htmlbars-inline-precompile'
-  const tmpl: TemplateFactory
-  export default tmpl
-}
