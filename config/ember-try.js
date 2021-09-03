bf91543e3525f8621563e57d8579706e3c703a25
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2021-09-03 11:05:45 +0100

    chore: remove old references to Ember.js canary

--- config/ember-try.js
+++ config/ember-try.js
@@ -30,14 +30,6 @@ module.exports = async function () {
           },
         },
       },
-      // {
-      //   name: 'ember-canary',
-      //   npm: {
-      //     devDependencies: {
-      //       'ember-source': await getChannelURL('canary'),
-      //     },
-      //   },
-      // },
       // The default `.travis.yml` runs this scenario via `npm test`,
       // not via `ember try`. It's still included here so that running
       // `ember try:each` manually or from a customized CI config will run it
