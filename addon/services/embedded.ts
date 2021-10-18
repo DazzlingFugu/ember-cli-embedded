641da4af85974cf8ef94c396c8bd97641d7d3da4
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2021-10-18 21:29:08 +0100

    chore: ensure factory `config:embedded` exists

--- addon/services/embedded.ts
+++ addon/services/embedded.ts
@@ -1,14 +1,22 @@
 import ObjectProxy from '@ember/object/proxy'
 import { getOwner } from '@ember/application'
+import { assert } from '@ember/debug'
 
 export default class EmbeddedService extends ObjectProxy {
 
   constructor() {
     super(...arguments) // eslint-disable-line prefer-rest-params
 
-    this.content = getOwner(this)
-      .factoryFor('config:embedded')
-      .class
+    const factoryName = 'config:embedded'
+    const factory: Record<string, unknown> | undefined = getOwner(this).factoryFor(factoryName)
+
+    assert(
+      `The factory "${factoryName}" could not be found.`,
+      typeof factory === 'object'
+    )
+
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    this.content = factory.class as object
   }
 
 }
