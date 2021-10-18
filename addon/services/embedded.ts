6e0717acd85fb3c21f7765ba6c2fb04e1aa37c3c
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2021-10-18 12:33:28 +0100

    refactor: from classic class to native JS class
    And declare the Service in the registry of Services.

--- addon/services/embedded.ts
+++ addon/services/embedded.ts
@@ -1,10 +1,21 @@
 import ObjectProxy from '@ember/object/proxy'
 import { getOwner } from '@ember/application'
 
-// eslint-disable-next-line ember/no-classic-classes
-export default ObjectProxy.extend({
-  init(...args: Array<Record<string, unknown>>): void {
-    this.content = getOwner(this).factoryFor('config:embedded').class
-    this._super(...args)
-  },
-})
+export default class EmbeddedService extends ObjectProxy {
+
+  constructor() {
+    super(...arguments) // eslint-disable-line prefer-rest-params
+
+    this.content = getOwner(this)
+      .factoryFor('config:embedded')
+      .class
+  }
+
+}
+
+// DO NOT DELETE: this is how TypeScript knows how to look up your services.
+declare module '@ember/service' {
+  interface Registry {
+    embedded: EmbeddedService;
+  }
+}
