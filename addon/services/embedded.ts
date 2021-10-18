e5452779705e551a1f9afb52b2012039cb184215
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2021-10-18 12:33:28 +0100

    refactor: remove key `isServiceFactory`
    1. Remove key `isServiceFactory`
    
    This key does not seem required anymore. Without it the tests still pass
    without any warning and a consuming application also does not get any
    warning when using this add-on.
    
    This key seems to be a relic from Ember.js <= v2, where it was required
    when a Servive was extending `Ember.ObjectProxy` instead of `Service`.
    
    See:
    - https://github.com/DockYard/ember-cart/pull/21
    - https://github.com/emberjs/ember.js/search?q=isServiceFactory&type=issues
    
    2. Condense export of the Service
    
    Using a `const` was already not necessary before, we could have chained
    `reopenClass()` right after the `ObjectProxy.extend()`.

--- addon/services/embedded.ts
+++ addon/services/embedded.ts
@@ -2,15 +2,9 @@ import ObjectProxy from '@ember/object/proxy'
 import { getOwner } from '@ember/application'
 
 // eslint-disable-next-line ember/no-classic-classes
-const configService = ObjectProxy.extend({
+export default ObjectProxy.extend({
   init(...args: Array<Record<string, unknown>>): void {
     this.content = getOwner(this).factoryFor('config:embedded').class
     this._super(...args)
   },
 })
-
-configService.reopenClass({
-  isServiceFactory: true,
-})
-
-export default configService
