36ab14e6506511397f555f3ab6ea9bc0432616a4
Nathanael Dekeister <nathanael_dekeister@ultimatesoftware.com>
2022-08-10 11:50:34 +0200

    refactor: dont return on instance-initializer initialize

--- addon/instance-initializers/embedded.ts
+++ addon/instance-initializers/embedded.ts
@@ -6,7 +6,7 @@ export function initialize(appInstance: ApplicationInstance): void {
   const appConf = appInstance.resolveRegistration('config:environment').APP
   const embedConf = appInstance.resolveRegistration('config:embedded')
 
-  return Object.assign(appConf, embedConf)
+  Object.assign(appConf, embedConf)
 }
 
 export default {
