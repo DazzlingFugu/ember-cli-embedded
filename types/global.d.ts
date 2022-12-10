7f8fd98121209d56bd79e1ec41dde031c186b7ea
MrChocolatine <47531779+MrChocolatine@users.noreply.github.com>
2022-12-10 18:03:13 +0000

    build: install `ember-cli-typescript@5.2.1`

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,6 +1,7 @@
 // Types for compiled templates
 declare module 'ember-cli-embedded/templates/*' {
-  import { TemplateFactory } from 'htmlbars-inline-precompile'
+  import { TemplateFactory } from 'ember-cli-htmlbars'
+
   const tmpl: TemplateFactory
   export default tmpl
 }
